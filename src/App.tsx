978ccd8308ac8e52123fa7b2a5514d82a97a1732
Al-Esku <alex.holton@yandex.com>
2025-01-31 03:24:30 +1300

    Styled export to XML button

--- src/App.tsx
+++ src/App.tsx
@@ -536,14 +536,21 @@ function App() {
                     <p className={"my-2 italic w-1/3 text-xs print:hidden"}>Third Tiebreaker: Strength of Victory, the
                         sum of
                         points scored by people you beat</p>
-                    <button onClick={() => print("table")} className={"border rounded px-1 mt-2 print:hidden flex"}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                             stroke="currentColor" className="size-6 w-4 h-4 mt-auto mb-auto">
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                  d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z"/>
+                    <div className={"flex"}>
+                        <button onClick={() => print("table")} className={"border rounded px-1 mt-2 print:hidden flex"}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6 w-4 h-4 mt-auto mb-auto">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z"/>
+                            </svg>
+                            <span className={"ml-1 mb-0.5"}>Print Table</span></button>
+                        <button onClick={() => exportToXML("results.xml")}
+                                className={"border rounded px-2 mt-2 ml-2 print:hidden flex"}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 w-4 h-4 mt-auto mb-auto mr-1">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                         </svg>
-                        <span className={"ml-1 mb-0.5"}>Print Table</span></button>
-                    <button onClick={() => exportToXML("results.xml")} className={"print:hidden"}>Export to XML</button>
+                            Export to XML
+                        </button>
+                    </div>
                     <button onClick={randomSeed}
                             className={"border rounded px-2 mt-6 print:hidden"}>{started ? "Shuffle Bouts" : "Start Round"}</button>
                     <button onClick={resetPairings}
@@ -554,7 +561,7 @@ function App() {
         <div className={printTarget === "bouts" ? "print-visible" : "print:hidden"}>
             {started ? <p className={"mt-8 font-semibold "}>Round {rounds} Bouts</p> : ""}
             <form id={"roundForm"} onSubmit={endRound} className={"m-2 "}>
-                {rounds > 0 && bouts[rounds-1].map(bout => (
+                {rounds > 0 && bouts[rounds - 1].map(bout => (
                     <div className={"p-4 print:w-full"}>
                         <div className={"inline-block w-1/3 mr-4 print:w-2/5"}>
                             <input className={"peer hidden"} type='radio' value={bout.fencer1.id}
