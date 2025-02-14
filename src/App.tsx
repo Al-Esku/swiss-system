824e59097f5cf320088011a33fede59d87350e0a
Al-Esku <alex.holton@yandex.com>
2025-02-15 02:30:45 +1300

    Added ability to export seedings to a csv file

--- src/App.tsx
+++ src/App.tsx
@@ -74,6 +74,22 @@ function App() {
       document.body.removeChild(element)
   }
 
+    const exportToCSV = (filename: string) => {
+        const resultData = `
+        ${table.map(fencer => {
+            return `${fencer.rank},${fencer.firstName},${fencer.lastName.toUpperCase()},${fencer.gender}`
+        }).join("\n")}
+      `
+        let element = document.createElement("a")
+        element.setAttribute("href", "data:text/csv;charset=utf-8," + encodeURIComponent(resultData))
+        element.setAttribute("download", filename)
+
+        element.style.display = "none"
+        document.body.appendChild(element)
+        element.click()
+        document.body.removeChild(element)
+    }
+
   const randomSeed = () => {
       setBye(undefined)
       if (fencers.length >= 2) {
@@ -386,11 +402,10 @@ function App() {
 
   return (
     <div className={"grid lg:grid-cols-2"}>
-
         <div className={"m-8"}>
             <Dialog.Root>
                 <Dialog.Trigger>
-                    <button className={"border rounded px-2 ml-2 print:hidden"}>Add fencers</button>
+                    <button className={"border rounded px-2 print:hidden"}>Add fencers</button>
                 </Dialog.Trigger>
                 <Dialog.Portal>
                     <Dialog.Overlay className={"bg-gray-600 opacity-80 fixed inset-0 z-30"} />
@@ -599,10 +614,22 @@ function App() {
                             </svg>
                             <span className={"ml-1 mb-0.5"}>Print Table</span></button>
                         <button onClick={() => exportToXML("results.xml")}
-                                className={"border rounded px-2 mt-2 ml-2 print:hidden flex"}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 w-4 h-4 mt-auto mb-auto mr-1">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                        </svg>
+                                className={"border rounded px-2 mt-2 ml-2 print:hidden flex"}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6 w-4 h-4 mt-auto mb-auto mr-1">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"/>
+                            </svg>
                             Export to XML
+                        </button>
+                        <button onClick={() => exportToCSV("seedings.csv")}
+                                className={"border rounded px-2 mt-2 ml-2 print:hidden flex"}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-6 w-4 h-4 mt-auto mb-auto mr-1">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"/>
+                            </svg>
+                            Export to CSV
                         </button>
                     </div>
                     <button onClick={randomSeed}
